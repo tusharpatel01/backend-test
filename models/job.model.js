b678c9165b06e6a633b5d2c9c77052c404a2718c
tushar patel <148350964+tusharpatel01@users.noreply.github.com>
2024-12-27 00:45:54 +0530

    Update job.model.js

--- models/job.model.js
+++ models/job.model.js
@@ -1,52 +1,28 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    requirements: [{
-        type: String
-    }],
-    salary: {
-        type: Number,
-        required: true
-    },
-    experienceLevel:{
-        type:Number,
-        required:true,
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    jobType: {
-        type: String,
-        required: true
-    },
-    position: {
-        type: Number,
-        required: true
-    },
-    company: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Company',
-        required: true
-    },
-    created_by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    applications: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Application',
-        }
-    ]
+const companySchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  description:{
+    type:String,
+  },
+  website:{
+    type:String,
+  },
+  location:{
+    type:String,
+  },
+  logo:{
+    type:String, //url of company logo
+  },
+  userId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+  }
 },{timestamps:true});
-export const Job = mongoose.model("Job", jobSchema);
+
+export const Company =  mongoose.model("Company", companySchema);
