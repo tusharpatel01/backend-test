d808b6f29254b00b95fe65a214e22db9491042ec
tushar patel <148350964+tusharpatel01@users.noreply.github.com>
2025-01-28 07:00:27 +0530

    Update isAuthenticated.js

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -6,11 +6,11 @@ const isAuthenticated = async (req, res, next) => {
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
-                message: "User not authenticated",
+                message: "User not authenticated you required authentication",
                 success: false,
             })
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        const decode =  jwt.verify(token, process.env.SECRET_KEY);
         if(!decode){
             return res.status(401).json({
                 message:"Invalid token",
